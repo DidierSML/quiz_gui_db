98adc51df1622aaf498ed83b1a7489c84d3badd9
DidierSML <didiersmaldonad@gmail.com>
2024-04-05 17:15:55 -0500

    1st Commit -Quiz gui db- Game

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import screens.TitleScreenGui;
+
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        //Ensures Swing GUI tasks are executed on the event dispatch thread (EDT)
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                //Create and display the title screen gui window
+                new TitleScreenGui().setVisible(true);
+            }
+        });
+    }
+}
---
+++ src/main/java/screens/TitleScreenGui.java
@@ -0,0 +1,29 @@
+package screens;
+
+import javax.swing.*;
+
+public class TitleScreenGui extends JFrame {
+
+    public TitleScreenGui(){
+        //call the constructor of the superclass with the title of "Title Screen"
+        super("Title Screen");
+
+        //set the size of the JFrame to 400 px wide and 565 px tall
+        setSize(400,565);
+
+        //set the layout manager of the frame to null, allowing manual positioning of the components
+        setLayout(null);
+
+        //set the frame to be centered on the screen when displayed
+        setLocationRelativeTo(null);
+
+        //disable resizing of the frame by the user
+        setResizable(false);
+
+        //set the default close operation of the frame to exit after the application has been closed
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+
+    }
+}
